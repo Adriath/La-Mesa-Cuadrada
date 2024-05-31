751e0cb88c2b4444a969d13b7fb2c531a40103e6
Adriath <adrianarjonabravo@gmail.com>
2024-06-01 01:20:51 +0200

    Creada propuesta de clase Response

--- CONTROL/class/Response.php
+++ CONTROL/class/Response.php
@@ -2,6 +2,58 @@
 
 class Response {
 
+    private $response = [
+        'status' => "ok",
+        'result' => array()
+    ];
 
+    // 200
     
+    public function error_200($string = "Datos incorrectos") { // Envío correcto pero datos erróneos
+
+        $this->response['status'] = "error" ;
+        $this->response['result'] = array(
+            "error_id" => "200",
+            "error_msg" => $string
+        ) ;
+
+        return $this->response ;
+    }
+
+    // 400
+
+    public function error_405() { // Envío por método no permitido
+
+        $this->response['status'] = "error" ;
+        $this->response['result'] = array(
+            "error_id" => "405",
+            "error_msg" => "Metodo no permitido"
+        ) ;
+
+        return $this->response ;
+    }
+
+
+    public function error_400() { // Datos enviados incorrectos
+
+        $this->response['status'] = "error" ;
+        $this->response['result'] = array(
+            "error_id" => "400",
+            "error_msg" => "Datos enviados incompletos o con formato incorrecto"
+        ) ;
+
+        return $this->response ;
+    }
+
+
+    public function error_404() { // Recurso no encontrado
+
+        $this->response['status'] = "error" ;
+        $this->response['result'] = array(
+            "error_id" => "404",
+            "error_msg" => "El servidor no puede encontrar el recurso solicitado"
+        ) ;
+
+        return $this->response ;
+    }
 }

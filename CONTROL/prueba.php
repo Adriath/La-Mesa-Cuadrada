2f7bae90004dc84df331ecf07e6aec9f76969eff
Adriath <adrianarjonabravo@gmail.com>
2024-05-31 22:36:00 +0200

    Ya hay conexión con BBDD.

--- CONTROL/prueba.php
+++ CONTROL/prueba.php
@@ -3,3 +3,7 @@
 require_once '../MODELO/BBDD.php' ;
 
 $conexion = new BBDD() ;
+
+$query = "SELECT * FROM juego" ;
+
+print_r($conexion->obtenerDatos($query)) ;
--- MODELO/BBDD.php
+++ MODELO/BBDD.php
@@ -35,4 +35,55 @@ class BBDD{
         
         return json_decode($jsondata, true) ; // True convierte en array asociativo
     }
+
+
+    private function convertirUTF8($array){
+
+        array_walk_recursive($array, function(&$item, $key){
+
+            if(!mb_detect_encoding($item, 'utf-8', true)){
+
+                $item = mb_convert_encoding($item, 'UTF-8') ;
+            }
+        }) ;
+
+        return $array ;
+    }
+
+    public function obtenerDatos($sqlstr){
+
+        $results = $this->conexion->query($sqlstr) ;
+        $resultArray = array() ;
+
+        foreach ($results as $key) {
+            
+            $resultArray[] = $key ;
+        }
+
+        return $this->convertirUTF8($resultArray) ;
+    }
+
+    public function nonQuery($sqlstr){
+
+        $results = $this->conexion->query($sqlstr) ;
+
+        return $this->conexion->affected_rows ;
+    }
+
+
+    public function nonQueryId($sqlstr){
+
+        $results = $this->conexion->query($sqlstr) ;
+        $filas = $this->conexion->affected_rows ;
+
+        if ($filas >= 1)
+        {
+            return $this->conexion->insert_id ;
+        }
+        else
+        {
+            return 0 ;
+        }
+    }
+
 }

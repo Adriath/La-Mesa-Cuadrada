12dce71543fcf22f223b0c6300a21d324784197e
Adriath <adrianarjonabravo@gmail.com>
2024-06-01 20:35:07 +0200

    Hasta aquí la creación de tokens y autentificación (no implementadas sesiones). Sigue: CRUD.

--- auth.php
+++ auth.php
@@ -8,11 +8,32 @@ $_respuestas = new Response() ;
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') { // Si va por método POST
 
+    // Recibir datos
+
     $postBody = file_get_contents('php://input') ; // Obtener datos del body
+
+    // Enviamos los datos al manejador
+
     $datosArray = $_auth->login($postBody) ; // Llamar al login
-    print_r(json_encode($datosArray)) ; // Devolver datos
+
+    // Devolvemos una respuesta
+
+    header('Content-Type: application/json') ;
+    if (isset($datosArray["result"]["error_id"])) {
+        
+        $responseCode = $datosArray["result"]["error_id"] ;
+        http_response_code($responseCode) ;
+    }
+    else{
+
+        http_response_code(200) ;
+    }
+    echo json_encode($datosArray) ; // Devolver datos
 }
 else{
 
-    echo "Método no permitido" ;
+    header('Content-Type: application/json') ;
+    $datosArray = $_respuestas->error_405() ;
+    
+    echo json_encode($datosArray) ;
 }

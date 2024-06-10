111835a08d2e277d55f3f7a8559af8ac2613bdf5
Adriath <adrianarjonabravo@gmail.com>
2024-06-10 15:53:04 +0200

    Voy a probar a unificiar los keyup.

--- CONTROL/login.php
+++ CONTROL/login.php
@@ -1,3 +1,35 @@
 <?php
 
 echo "Esta es la página Login.php" ;
+
+$nombre = $_POST['nombreUsuario'] ;
+$email = $_POST['usuario'] ;
+$password = $_POST['password'] ;
+
+$nombreValido = false ;
+$emailValido = false ;
+$passwordValido = false ;
+
+$nombreYaExiste = false ;
+$emailYaExiste = false ;
+
+$formularioValido = false ;
+
+// ------------- VALIDACIONES DE FORMATO ---------------
+
+function validarEmail($email) {
+
+    return filter_var($email, FILTER_VALIDATE_EMAIL) ;
+}
+
+$email1 = "ejemplo@dominio.com";
+$email2 = "incorrecto";
+
+if (validarEmail($email1)) {
+ echo "El correo electrónico $email1 es válido.";
+} else {
+ echo "El correo electrónico $email1 no es válido.";
+}
+
+// Salida: El correo electrónico ejemplo@dominio.com es válido.
+

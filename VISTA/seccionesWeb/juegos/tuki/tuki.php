e1a460f289d4dd3c7b9d33797d37f364bc89a290
Adriath <adrianarjonabravo@gmail.com>
2024-06-11 19:23:15 +0200

    Voy a hacer la clase PartidaActual en Netbeans.

--- VISTA/seccionesWeb/juegos/tuki/tuki.php
+++ VISTA/seccionesWeb/juegos/tuki/tuki.php
@@ -13,7 +13,7 @@
 
     <div class="container-fluid rounded-5 border border-5 border-warning position-relative" style="height: 325px; width: 100%;">
   <div class="bg-image position-absolute top-0 start-0 w-100 h-100" style="background-image: url('http://localhost/La_Mesa_Cuadrada/VISTA/img/tapete_verde.png'); background-size: cover; background-position: center; opacity: 0.9;"></div>
-  <div class="position-relative" style="z-index: 1;">
+  <div class="position-relative" id="tapete" style="z-index: 1;">
     
     <!-- AQUÍ VA EL CÓDIGO -->
 
